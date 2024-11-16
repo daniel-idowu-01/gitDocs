e82ecbc4b308710449279d02c5dffebafe8f527e
daniel-idowu-01 <danielidowu414@gmail.com>
2024-11-16 23:25:24 +0100

    add: test gemini implementation

---
+++ server/src/services/aiService.js
@@ -0,0 +1,5 @@
+export default class AIService {
+  constructor() {}
+
+  async generateDocumentation() {}
+}
--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,9 +1,11 @@
 import GithubService from "./githubService.js";
 import MarkdownService from "./markdownService.js";
 import PdfGenerator from "./pdfGenerator.js";
+import AIService from "./aiService.js";
 
 const githubService = new GithubService();
 const markdownService = new MarkdownService();
 const pdfGenerator = new PdfGenerator();
+const aiService = new AIService();
 
-export { githubService, markdownService, pdfGenerator };
+export { githubService, markdownService, pdfGenerator, aiService };
